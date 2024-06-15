5c772751e32c988ef71c1de914e2a50bf67f0600
JAN <janstefanlebar@gmail.com>
2024-06-15 21:29:14 +0200

    update

--- app/components/Edit.jsx
+++ app/components/Edit.jsx
@@ -1,4 +1,3 @@
-
 "use client";
 import React, { useState } from 'react';
 
@@ -20,65 +19,94 @@ const Characters = ({ initialAttributes, onChange }) => {
     setIsEditMode(!isEditMode); // Toggle edit mode
   };
 
+  const inputStyle = {
+    backgroundColor: isEditMode ? 'white' : 'transparent',
+    border: isEditMode ? '1px solid black' : 'none',
+  };
+
   return (
     <div>
       <form onSubmit={(e) => e.preventDefault()}>
-      <p>Name:</p>
-        <input
-          type="text"
-          value={attributes.name}
-          onChange={handleChange("name")}
-          disabled={!isEditMode}
-        />
-      <p>Height:</p>
-        <input
-          type="text"
-          value={attributes.height}
-          onChange={handleChange("height")}
-          disabled={!isEditMode}
-        />
-      <p>Mass:</p>
-        <input
-          type="text"
-          value={attributes.mass}
-          onChange={handleChange("mass")}
-          disabled={!isEditMode}
-        />
-      <p>Hair Color:</p>
-        <input
-          type="text"
-          value={attributes.hair_color}
-          onChange={handleChange("hair_color")}
-          disabled={!isEditMode}
-        />
-      <p>Skin Color:</p>
-        <input
-          type="text"
-          value={attributes.skin_color}
-          onChange={handleChange("skin_color")}
-          disabled={!isEditMode}
-        />
-      <p>Eye Color:</p>
-        <input
-          type="text"
-          value={attributes.eye_color}
-          onChange={handleChange("eye_color")}
-          disabled={!isEditMode}
-        />
-      <p>Birth Year:</p>
-        <input
-          type="text"
-          value={attributes.birth_year}
-          onChange={handleChange("birth_year")}
-          disabled={!isEditMode}
-        />
-      <p>Gender</p>
-        <input
-          type="text"
-          value={attributes.gender}
-          onChange={handleChange("gender")}
-          disabled={!isEditMode}
-        />
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <p style={{ margin: '0 10px 0 0' }}>Name:</p>
+          <input
+            type="text"
+            value={attributes.name}
+            onChange={handleChange("name")}
+            disabled={!isEditMode}
+            style={inputStyle}
+          />
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <p style={{ margin: '0 10px 0 0' }}>Height:</p>
+          <input
+            type="text"
+            value={attributes.height}
+            onChange={handleChange("height")}
+            disabled={!isEditMode}
+            style={inputStyle}
+          />
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <p style={{ margin: '0 10px 0 0' }}>Mass:</p>
+          <input
+            type="text"
+            value={attributes.mass}
+            onChange={handleChange("mass")}
+            disabled={!isEditMode}
+            style={inputStyle}
+          />
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <p style={{ margin: '0 10px 0 0' }}>Hair Color:</p>
+          <input
+            type="text"
+            value={attributes.hair_color}
+            onChange={handleChange("hair_color")}
+            disabled={!isEditMode}
+            style={inputStyle}
+          />
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <p style={{ margin: '0 10px 0 0' }}>Skin Color:</p>
+          <input
+            type="text"
+            value={attributes.skin_color}
+            onChange={handleChange("skin_color")}
+            disabled={!isEditMode}
+            style={inputStyle}
+          />
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <p style={{ margin: '0 10px 0 0' }}>Eye Color:</p>
+          <input
+            type="text"
+            value={attributes.eye_color}
+            onChange={handleChange("eye_color")}
+            disabled={!isEditMode}
+            style={inputStyle}
+          />
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <p style={{ margin: '0 10px 0 0' }}>Birth Year:</p>
+          <input
+            type="text"
+            value={attributes.birth_year}
+            onChange={handleChange("birth_year")}
+            disabled={!isEditMode}
+            style={inputStyle}
+          />
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <p style={{ margin: '0 10px 0 0' }}>Gender:</p>
+          <input
+            type="text"
+            value={attributes.gender}
+            onChange={handleChange("gender")}
+            disabled={!isEditMode}
+            style={inputStyle}
+          />
+        </div>
         <button type="button" onClick={handleEditClick}>
           {isEditMode ? "Save" : "Edit"}
         </button>
