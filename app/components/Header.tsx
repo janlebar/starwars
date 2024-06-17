a08c2852b76a7e30dd1c1f5cfb39ceff7de7e96c
JAN <janstefanlebar@gmail.com>
2024-06-17 13:09:24 +0200

    update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -7,10 +7,10 @@ const Header = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <header className="h-20 lg:h-16 md:flex justify-between items-center" style={{ maxWidth: '761px', height: '127px' }}>
           <div className="flex items-center space-x-8"> 
-            <Link href="/" className="font-sans text-2xl font-bold leading-6 text-left">
+            <Link href="/" className="font-sans text-2xl font-bold leading-6 text-left text-white">
               Upcoming
             </Link>
-            <Link href="/" className="font-sans text-2xl font-bold leading-6 text-left">
+            <Link href="/" className="font-sans text-2xl font-bold leading-6 text-left text-white">
               Legacy
             </Link>
           </div>
@@ -18,10 +18,10 @@ const Header = () => {
             <Image src="/logo.png" alt="Logo" width={210} height={127} />
           </div>
           <div className="hidden md:flex items-center space-x-8"> 
-            <Link href="/" className="font-sans text-2xl font-bold leading-6 text-left">
+            <Link href="/" className="font-sans text-2xl font-bold leading-6 text-left text-white">
               Merch
             </Link>
-            <Link href="/about" className="font-sans text-2xl font-bold leading-6 text-left">
+            <Link href="/about" className="font-sans text-2xl font-bold leading-6 text-left text-white">
               About
             </Link>
           </div>
